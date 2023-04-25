ea9d3b941539665b5ae8ee051f1cfda16efcf953
Enzo Medaglia <enzomedaglia@gmail.com>
2023-04-25 10:44:39 -0300

    Coding Exercise 15 Iterator Pattern

---
+++ Coding Exercise 15 Iterator Pattern.py
@@ -0,0 +1,23 @@
+"""
+Iterator Coding Exercise
+Given the following definition of a Node , please implement preorder traversal right inside Node.
+The sequence returned should be the sequence of values, not their containing nodes.
+"""
+
+
+class Node:
+    def __init__(self, value, left=None, right=None):
+        self.right = right
+        self.left = left
+        self.value = value
+
+        self.parent = None
+
+        if left:
+            self.left.parent = self
+        if right:
+            self.right.parent = self
+
+    def traverse_preorder(self):
+        ...
+        # todo - return inorder values (not Nodes)
